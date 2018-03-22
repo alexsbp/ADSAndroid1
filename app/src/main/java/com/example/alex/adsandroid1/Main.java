91d5d0f202ea63d0c80369c72cc91a33ca1a6095
Nikolaj Vestergaard Hentze <niko0654@student.eadania.dk>
2018-03-22 13:32:12 +0100

    Fuck dig alex

--- app/src/main/java/com/example/alex/adsandroid1/Main.java
+++ app/src/main/java/com/example/alex/adsandroid1/Main.java
@@ -1,13 +1,48 @@
 package com.example.alex.adsandroid1;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        // Create a listener
+            SensorEventListener gyroscopeSensorListener = new SensorEventListener() {
+                @Override
+                public void onSensorChanged(SensorEvent sensorEvent) {
+                    // More code goes here
+                    if(sensorEvent.values[2] > 0.5f) { // anticlockwise
+                        getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                    } else if(sensorEvent.values[2] < -0.5f) { // clockwise
+                        getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                    }
+                }
+
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int i) {
+                }
+        };
+
+
+// Register the listener
+        sensorManager.registerListener(gyroscopeSensorListener,
+                gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+
+
+
 }
